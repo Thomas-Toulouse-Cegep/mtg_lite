ae8b7ae8183143d7949578c90b089504a4460c92
maxance-gusse-cegep <2134980@cegepba.qc.ca>
2022-11-09 09:48:52 -0500

    Update Sorcery.cs
    Create factory for Sorcery

--- mtg_lite/Models/Cards/Sorcery.cs
+++ mtg_lite/Models/Cards/Sorcery.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using mtg_lite.Models.Cards.Sorcerys;
+using MTGO_lite.Models.Manas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,29 @@ using System.Threading.Tasks;
 
 namespace mtg_lite.Models.Cards
 {
-    internal class Sorcery
+    internal class Sorcery : Card
     {
+        public Sorcery(string name, Mana manaCost, Bitmap picture) : base(name, manaCost, picture)
+        {
+        }
+
+        public static Card SorceryFactory(string Type)
+        {
+            Card CardType = null;
+
+            if (Type.ToLower().Equals("blightning"))
+            {
+                CardType = new Blightning("blightning", );
+            }
+            else if (Type.ToLower().Equals("chain_lightning"))
+            {
+                CardType = new ChainLightning("chain_lightning");
+            }
+            else if (Type.ToLower().Equals("clone_legion"))
+            {
+                CardType = new CloneLegion("clone_legion");
+            }
+            return CardType;
+        }
     }
 }

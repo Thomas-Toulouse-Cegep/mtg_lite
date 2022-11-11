5a116db5f1210f15d67b45457beaaa123e72e66d
Thomas-Toulouse-Cegep <2134063@cegepba.qc.ca>
2022-11-11 15:18:21 -0500

    nom added

---
+++ mtg_lite/Models/Zones/Hand.cs
@@ -0,0 +1,22 @@
+﻿using mtg_lite.Models.Cards;
+using mtg_lite.Models.Players;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mtg_lite.Models.Zones
+{
+    internal class Hand : Zone
+    {
+        public Hand(List<Card> cards, Player player) : base(cards, player)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{"Hand"} ({cards.Count})";
+        }
+    }
+}

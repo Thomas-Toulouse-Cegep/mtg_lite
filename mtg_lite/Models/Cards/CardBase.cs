627dc52a8e4ee9941500e788c8905050a2a103e6
Thomas-Toulouse-Cegep <2134063@cegepba.qc.ca>
2022-11-23 10:27:27 -0500

    sa rotate

---
+++ mtg_lite/Models/Cards/CardBase.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+namespace mtg_lite.Models.Cards
+{
+    [DebuggerDisplay("{Name}")]
+    public class CardBase
+    {
+        public event EventHandler<bool>? TappedChanged;
+    }
+}
